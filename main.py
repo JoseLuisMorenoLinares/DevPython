921a0c288ca2a80561001ccffeae9cebf12e3c26
joseluismorenolinares <jose_moreno_linares@hotmail.com>
2024-10-16 13:02:23 +0200

    ampliacion juego piedra, papel, tijeras, lagarto y spock

--- main.py
+++ main.py
@@ -1140,7 +1140,7 @@ class Participant:
             "paper": 1,
             "scissor": 2
         }
-        return switcher[self.choice]
+        return switcher[self.choice.lower()]
     def incrementPoint(self):
         self.points += 1
 
@@ -1202,6 +1202,7 @@ game = Game()
 game.start()
 '''
 
+'''
 # JUEGO DE PIEDRA, PAPEL O TIJERAS (EN ESPANYOL)
 import os
 os.system("cls")
@@ -1220,7 +1221,7 @@ class Participant:
             "papel": 1,
             "tijeras": 2
         }
-        return switcher[self.choice]
+        return switcher[self.choice.lower()]
     def incrementPoint(self):
         self.points += 1
 
@@ -1284,5 +1285,202 @@ class Game:
 
 game = Game()
 game.start()
-# comentario
+'''
 
+'''
+# Ejercicio: Ampliación de la implementación del juego con reglas agregadas (INGLES)
+import os
+os.system("cls")
+
+class Participant:
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
+        self.choice = ""
+    def choose(self):
+        self.choice = input("{name}, select rock, paper, scissor, lizard or spock: ".format(name= self.name))
+        print("{name} selects {choice}".format(name=self.name, choice = self.choice))        
+    def toNumericalChoice(self):
+        switcher = {
+             "rock": 0,
+             "paper": 1,
+             "scissor": 2,
+             "lizard": 3,
+             "spock": 4
+        }        
+        return switcher[self.choice.lower()]
+    def incrementPoint(self):
+        self.points += 1
+
+class GameRound:
+    def __init__(self, p1, p2):
+        self.rules = [
+            [0, -1, 1, 1, -1],
+            [1, 0, -1, -1, 1],
+            [-1, 1, 0, 1, -1],
+            [-1, 1, -1, 0, 1],
+            [1, -1, 1, -1, 0]
+        ]
+        p1.choose()
+        p2.choose()
+        result = self.compareChoices(p1,p2)
+        print("Round resulted in a {result}".format(result = self.getResultAsString(result) ))
+        if result > 0:
+           p1.incrementPoint()
+        elif result < 0:
+           p2.incrementPoint()
+    def compareChoices(self, p1, p2):
+        return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
+    def awardPoints(self):
+        print("implement")
+    def getResultAsString(self, result):
+        res = {
+            0: "draw",
+            1: "win",
+            -1: "loss"
+        }       
+        return res[result]        
+
+class Game:
+    def __init__(self):
+        self.endGame = False
+        self.participant = Participant("Spock")
+        self.secondParticipant = Participant("Kirk")
+    def start(self):
+        while not self.endGame:
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+    def checkEndCondition(self):
+        answer = input("Continue game y/n: ")
+        if answer.lower() == 'y':
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+        else:
+            print("Game ended, {p1name} has {p1points}, and {p2name} has {p2points}".format(p1name = self.participant.name, p1points= self.participant.points, p2name=self.secondParticipant.name, p2points=self.secondParticipant.points))
+            self.determineWinner()
+            self.endGame = True        
+    def determineWinner(self):
+        resultString = "It's a Draw"
+        if self.participant.points > self.secondParticipant.points:
+            resultString = "Winner is {name}".format(name=self.participant.name)
+        elif self.participant.points < self.secondParticipant.points:
+            resultString = "Winner is {name}".format(name=self.secondParticipant.name)
+        print(resultString)
+
+game = Game()
+game.start()
+'''
+
+'''
+# Ejercicio: Ampliación de la implementación del juego con reglas agregadas (INGLES)
+import os
+os.system("cls")
+
+class Participant:
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
+        self.choice = ""
+    def choose(self):
+        self.choice = input("{name}, seleccione piedra, papel, tijeras, lagarto o spock: ".format(name= self.name))
+        print("{name} selecciono {choice}".format(name=self.name, choice = self.choice))
+    def toNumericalChoice(self):
+        switcher = {
+            "piedra": 0,
+            "papel": 1,
+            "tijeras": 2,
+            "lagarto": 3,
+            "spock": 4
+        }
+        return switcher[self.choice.lower()]
+    
+    def incrementPoint(self):
+        self.points += 1
+
+class GameRound:
+    def __init__(self, p1, p2):
+        self.rules = [
+            [0, -1, 1, 1, -1],
+            [1, 0, -1, -1, 1],
+            [-1, 1, 0, 1, -1],
+            [-1, 1, -1, 0, 1],
+            [1, -1, 1, -1, 0]
+        ]
+        p1.choose()
+        p2.choose()
+        result = self.compareChoices(p1,p2)
+        print("Resultado de la ronda en {result}".format(result = self.getResultAsString(result) ))
+        if result > 0:
+           p1.incrementPoint()
+        elif result < 0:
+           p2.incrementPoint()
+    def compareChoices(self, p1, p2):
+        return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
+    def awardPoints(self):
+        print("implement")
+    def getResultAsString(self, result):
+        res = {
+            0: "empate",
+            1: "victoria",
+            -1: "derrota"
+        }       
+        return res[result]        
+
+class Game:
+    def __init__(self):
+        self.endGame = False
+        #self.participant = Participant("Jose")
+        #self.secondParticipant = Participant("Antonio")
+        participante1 = input("Introduzca nombre primer participante: ")
+        self.participant = Participant(participante1)
+        participante2 = input("Introduzca nombre segundo participante: ")
+        self.secondParticipant = Participant(participante2)
+    def start(self):
+        while not self.endGame:
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+    def checkEndCondition(self):
+        answer = input("Continuar juego s/n: ")
+        if answer.lower() == 's':
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+        else:
+            print("Final del juego, {p1name} tiene {p1points} puntos, y {p2name} tiene {p2points} puntos".format(p1name = self.participant.name, p1points= self.participant.points, p2name=self.secondParticipant.name, p2points=self.secondParticipant.points))
+            self.determineWinner()
+            self.endGame = True        
+    def determineWinner(self):
+        resultString = "Es un Empate"
+        if self.participant.points > self.secondParticipant.points:
+            resultString = "El ganador es {name}".format(name=self.participant.name)
+        elif self.participant.points < self.secondParticipant.points:
+            resultString = "El ganador es {name}".format(name=self.secondParticipant.name)
+        print(resultString)
+
+game = Game()
+game.start()
+'''
+
+
+'''
+import random
+import os
+os.system("cls")
+
+def retornaEleccion():
+    switcher = {
+            1: "piedra",
+            2: "papel",
+            3: "tijeras",
+            4: "lagarto",
+            5: "spock"
+    }
+    return switcher[random.randint(1, 5)]
+
+def retornaEleccion2():
+    opciones = ["piedra", "papel", "tijeras", "lagarto", "spock"]
+    return opciones[random.randint(0, 4)]
+
+
+print(retornaEleccion())
+print(retornaEleccion2())
+'''
